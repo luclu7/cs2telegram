0f5b2325da3bfbacb81aa5ee1e9913a68ed4d824
Luclu7 <me@luclu7.fr>
2019-07-02 23:43:21 +0200

    initial commit/release

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/luclu7/cs2telegram
+
+go 1.12
+
+require (
+	github.com/PuerkitoBio/goquery v1.5.0 // indirect
+	github.com/mmcdole/gofeed v1.0.0-beta2
+	github.com/mmcdole/goxpp v0.0.0-20181012175147-0068e33feabf // indirect
+	golang.org/x/net v0.0.0-20190628185345-da137c7871d7
+	golang.org/x/text v0.3.2 // indirect
+	gopkg.in/tucnak/telebot.v2 v2.0.0-20190415090633-8c1c512262f2
+)
---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"github.com/mmcdole/gofeed"
+	tb "gopkg.in/tucnak/telebot.v2"
+	"io/ioutil"
+	"log"
+	"os"
+	"regexp"
+	"strings"
+	"time"
+)
+
+// RecipientType - RecipientType
+type RecipientType struct {
+	Channel string `json:"channel"`
+}
+
+// Recipient - Recipient
+func (x RecipientType) Recipient() string {
+	return x.Channel
+}
+
+func parseflux(url string) (feed *gofeed.Feed) {
+	parser := gofeed.NewParser()
+	feed, _ = parser.ParseURL(url)
+	return feed
+}
+
+func read(file string) string {
+	buf, _ := ioutil.ReadFile(file)
+	final := string(buf)
+	return final
+}
+
+func checkandpost(b *tb.Bot) {
+	log.Print("Downloading and parsing the RSS feed...")
+	feed := parseflux("https://www.commitstrip.com/fr/feed/")
+
+	comic := read("latestcomic.txt")
+
+	f, err := os.Create("latestcomic.txt")
+	defer f.Close()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	var toChannel tb.Recipient = RecipientType{Channel: "@commitstrip_fr"}
+	if comic == feed.Items[0].GUID {
+		log.Print("No new comic available")
+		f.WriteString(feed.Items[0].GUID)
+	} else {
+		log.Print("New comic available")
+		r, _ := regexp.Compile(`<img[^>]+src="([^">]+)"`)
+		matches := r.FindStringSubmatch(feed.Items[0].Content)
+		f.WriteString(feed.Items[0].GUID)
+		picture := &tb.Photo{File: tb.FromURL(matches[1])}
+		message := feed.Items[0].Title + " " + feed.Items[0].Link
+		b.Send(toChannel, message)
+		b.Send(toChannel, picture)
+
+	}
+}
+
+func main() {
+	log.Print("Starting...")
+	token := read("token.txt")
+	token = strings.TrimSuffix(token, "\n")
+	b, err := tb.NewBot(tb.Settings{
+		Token: token,
+		// You can also set custom API URL. If field is empty it equals to "https://api.telegram.org"
+		//URL:    "http://195.129.111.17:8012",
+		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
+	})
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	//checkandpost(b)
+	dateTicker := time.NewTicker(30 * time.Second)
+
+	for {
+		select {
+		case <-dateTicker.C:
+			checkandpost(b)
+		}
+	}
+
+}
